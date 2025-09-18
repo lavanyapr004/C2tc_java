ee0cb96617e6dda0dc37acb002ffa176333d8413
LAVANYA P R <LAVANYA P R@DESKTOP-8MOGRTF>
2025-09-18 12:49:14 +0530

    This is my first Java Project.

---
+++ c2tc_java/src/com/tns/c2tc_java/UserName.java
@@ -0,0 +1,31 @@
+package com.tns.c2tc_java;
+import java.util.Scanner;
+
+public class UserName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        Scanner input = new Scanner(System.in);
+
+		        // Taking string input
+		        System.out.println("Enter your name:");
+		        String n = input.nextLine();
+		        System.out.println("Hello " + n);
+
+		        // Taking integer input
+		        System.out.println("Enter your roll number:");
+		        if (input.hasNextInt()) {
+		            int rollNo = input.nextInt();
+		            System.out.println("The roll number is: " + rollNo);
+		        } else {
+		            System.out.println("Invalid input. Please enter an integer.");
+		        }
+
+		        input.close();
+		    }
+		
+
+	}
+
+
